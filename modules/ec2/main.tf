c0287c39e90d08430202bafafb42d3d9a596bc69
Munney84 <108026310+Munney84@users.noreply.github.com>
2022-11-02 19:37:00 -0400

    pub to private subnet

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "web_server01" {
   ami                    = var.ami
   instance_type          = var.instance_type
-  subnet_id              = var.public_subnets1_id
+  subnet_id              = var.private_subnets1_id
   vpc_security_group_ids = [var.vpc_security_group_id_1]
  
   key_name = var.key
